620c651ce79a8d26ed248d89fb56bd6a1ebff3ae
Nick Podratz <nick@podratz.de>
2025-04-22 11:50:50 +0200

    Remove unused imports

--- app/src/main/java/de/syntax_institut/syntaxnotes/ui/screens/NoteEditorScreen.kt
+++ app/src/main/java/de/syntax_institut/syntaxnotes/ui/screens/NoteEditorScreen.kt
@@ -4,9 +4,7 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextField

d311ad70d4dff264f60539be9e9c651879a315fa
Nick Podratz <nick@podratz.de>
2025-04-22 10:37:54 +0200

    Create NoteEditor with view model

---
+++ app/src/main/java/de/syntax_institut/syntaxnotes/ui/screens/NoteEditorScreen.kt
@@ -0,0 +1,60 @@
+package de.syntax_institut.syntaxnotes.ui.screens
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TopAppBar
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.viewmodel.compose.viewModel
+import de.syntax_institut.syntaxnotes.ui.viewmodels.NoteEditorViewModel
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun NoteEditorScreen(
+    modifier: Modifier = Modifier,
+    viewModel: NoteEditorViewModel = viewModel()
+) {
+    val text by viewModel.text.collectAsState()
+
+    Scaffold(
+        topBar = {
+            TopAppBar(title = {
+                Text("New Note")
+            })
+        }
+    ) { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize()
+                .padding(vertical = 24.dp)
+        ) {
+            TextField(
+                text,
+                label = {
+                    Text("Text")
+                },
+                onValueChange = { newText: String -> viewModel.updateText(newText) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(16.dp)
+            )
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun NoteEditorScreenPreview() {
+    NoteEditorScreen()
+}
---
+++ app/src/main/java/de/syntax_institut/syntaxnotes/ui/viewmodels/NoteEditorViewModel.kt
@@ -0,0 +1,15 @@
+package de.syntax_institut.syntaxnotes.ui.viewmodels
+
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class NoteEditorViewModel : ViewModel() {
+
+    private val _text = MutableStateFlow("")
+    val text = _text.asStateFlow()
+
+    fun updateText(newText: String) {
+        _text.value = newText
+    }
+}

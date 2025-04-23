bc5cc6ab7db75186a1d8986129cb25f25b05f605
Nick Podratz <nick@podratz.de>
2025-04-23 10:17:06 +0200

    Setup navigation

---
+++ app/src/main/java/de/syntax_institut/syntaxnotes/ui/AppStart.kt
@@ -0,0 +1,46 @@
+package de.syntax_institut.syntaxnotes.ui
+
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import de.syntax_institut.syntaxnotes.ui.screens.NoteEditorScreen
+import kotlinx.serialization.Serializable
+
+@Serializable
+object NoteListingRoute
+
+@Serializable
+object NoteEditorRoute
+
+@Composable
+fun AppStart() {
+    val navController = rememberNavController()
+
+    Scaffold { innerPadding ->
+        NavHost(
+            navController = navController,
+            startDestination = NoteListingRoute,
+            modifier = Modifier.padding(innerPadding)
+        ) {
+
+            composable<NoteListingRoute> {
+                // NoteListingScreen()
+            }
+
+            composable<NoteEditorRoute> {
+                NoteEditorScreen()
+            }
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun AppStartPreview() {
+    AppStart()
+}
